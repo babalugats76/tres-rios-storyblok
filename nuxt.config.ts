49040cc3bbf6b1dad3f7b272ddef2c5fe3f028e8
James Colestock <james@colestock.com>
2023-08-23 11:45:09 -0700

    Added tailwind and module configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  app: {
+    rootId: "tres-rios",
+    rootTag: "body",
+    head: {
+      title: "Tres Ríos",
+      meta: [
+        {
+          name: "description",
+          content: "Concierge Learning is Here",
+        },
+      ],
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    [
+      "@storyblok/nuxt",
+      {
+        accessToken: process.env.STORYBLOK_SECRET,
+        apiOptions: {
+          region: "us",
+        },
+      },
+    ],
+    "@nuxtjs/tailwindcss",
+  ],
+  // pageTransition: { name: "page", mode: "out-in" },
+  typescript: {
+    typeCheck: false,
+  },
+  debug: true,
+});
