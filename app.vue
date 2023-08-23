cf4f2606623fc6e2899a589714d4b29c06062be7
James Colestock <james@colestock.com>
2023-08-23 14:07:31 -0700

    added dummy app page

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
-<!-- <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,4 +12,4 @@
 
 <style scoped>
 
-</style> -->
+</style>
