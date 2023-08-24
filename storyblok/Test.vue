71c31b1dc01225f2d4583bc537ee590a32f06e1e
James Colestock <james@colestock.com>
2023-08-24 16:40:42 -0700

    Sample Article Richtext

--- storyblok/Test.vue
+++ storyblok/Test.vue
@@ -6,11 +6,34 @@ const rich = computed(() => renderRichText(props?.blok?.richtext))
 <template>
   <div
     v-editable="blok"
-    class="w-full p-12 bg-[#f7f6fd] rounded-[5px]"
+    class="md:columns-2 w-full p-12 bg-[#f7f6fd] rounded-[5px]"
   >
     <h3 class="text-2xl text-[#1d243d] font-bold">
       {{ blok?.name }}
     </h3>
-    <div v-html="rich" />
+    <div class="rich" v-html="rich" />
   </div>
 </template>
+
+<style lang="css">
+  .rich > p, h1, h2, img, blockquote {
+     padding: 1rem .25rem;
+  }
+
+  .rich > h1 {
+     font-size: 2rem;
+     font-weight: bold;
+     @apply font-heading;
+  }
+
+  .rich > blockquote {
+     font-size: 2rem;
+     font-weight: bold;
+     line-height: .9;
+     font-family: Georgia, 'Times New Roman', Times, serif;
+     font-style: italic;
+     @apply text-red-600;
+     @apply font-serif;
+  }
+
+</style>
