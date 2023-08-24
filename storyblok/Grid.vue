0962e3a357078529948faec51239324822a2415c
James Colestock <james@colestock.com>
2023-08-23 20:39:44 -0700

    grid fixes

--- storyblok/Grid.vue
+++ storyblok/Grid.vue
@@ -20,11 +20,7 @@ const classColumns = computed(() => ({
 </script>
 
 <template>
-  <div
-    v-editable="blok"
-    class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-center"
-    :class="classColumns"
-  >
+  <div v-editable="blok" class="container mx-auto grid gap-12 my-12 place-items-center" :class="classColumns">
     <StoryblokComponent v-for="b in blok?.columns" :key="b._uid" :blok="b" />
   </div>
 </template>
