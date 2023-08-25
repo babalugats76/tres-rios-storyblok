9fc0201cc90631268e34c52031c06b9927080578
James Colestock <james@colestock.com>
2023-08-25 13:49:14 -0700

    youtube storyblok component

---
+++ storyblok/YouTube.vue
@@ -0,0 +1,29 @@
+<script setup lang="js">
+  const props = defineProps({ blok: Object });
+  const video = computed(() => props?.blok?.id);
+
+  // const pageClasses = reactive({
+  //   container: true,
+  //   'overflow-hidden': true,
+  //   'mx-auto': true,
+  //   'px-4': padding,
+  // });
+</script>
+
+<template>
+  <div
+    v-editable="blok"
+    class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden"
+    style="padding-top: 56.25%"
+  >
+    <iframe
+      class="embed-responsive-item absolute bottom-0 left-0 right-0 top-0 h-full w-full"
+      :src="`https://www.youtube.com/embed/${video}`"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen=""
+    >
+      <StoryblokComponent :blok="b" />
+    </iframe>
+  </div>
+</template>
