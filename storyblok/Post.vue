6ea087e710fb4d36ba93a31cd9e0cc7070219621
James Colestock <james@colestock.com>
2023-08-24 21:23:02 -0700

    margin changes

--- storyblok/Post.vue
+++ storyblok/Post.vue
@@ -12,9 +12,9 @@ const pageClasses = reactive({
 });
 
 const blogDate = computed(() =>
-  new Date(props?.blok?.date || Date()).toLocaleDateString("default", {
-    month: "long",
+  new Date(props?.blok?.date || Date()).toLocaleDateString("en-GB", {
     day: "numeric",
+    month: "long",
     year: "numeric",
   }),
 );
@@ -23,26 +23,22 @@ const blogDate = computed(() =>
 <template>
   <div v-editable="blok" :class="pageClasses">
     <div class="md:max-w-2xl mx-auto">
+      <template v-for="c in categories" :key="c.id">
+        <span class="inline-block py-1 px-3 mb-4 mr-2 font-semibold text-orange-900 bg-orange-50 rounded-full">
+          {{ c.name }}
+        </span>
+      </template>
       <div class="blog__headline mb-10" v-html="richHeadline"></div>
       <div v-if="author">
         <p class="text-md font-sans font-thin tracking-tight text-gray-500 mb-8">
           {{ author.display_name }}&nbsp;&nbsp;&middot;&nbsp;&nbsp;{{ blogDate }}
         </p>
       </div>
-      <div v-if="categories">
-        <template v-for="c in categories" :key="c.id">
-          <div
-            class="inline-block py-1 px-3 text-xs leading-5 text-green-500 font-medium uppercase bg-green-100 rounded-full shadow-sm">
-            {{ c.name }}
-          </div>
-        </template>
+      <div v-if="blok?.image" class="h-64 mb-10">
+        <img :src="blok.image.filename" :title="blok.image.title" class="w-full h-full object-cover rounded-lg" alt="" />
       </div>
+      <div class="blog__text md:max-w-3xl mx-auto" v-html="richText"></div>
     </div>
-    <div v-if="blok?.image" class="mb-10 mx-auto max-w-max overflow-hidden rounded-lg">
-      <img :src="blok.image.filename" :title="blok.image.title" class="w-full h-[360px] lg:h-[450px] object-cover"
-        alt="" />
-    </div>
-    <div class="blog__text md:max-w-3xl mx-auto" v-html="richText"></div>
   </div>
 </template>
 
