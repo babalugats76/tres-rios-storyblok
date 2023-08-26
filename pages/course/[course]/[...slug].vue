bb8508215dee057c4f2e2ee96e99d4672871ccd9
James Colestock <james@colestock.com>
2023-08-26 05:38:16 -0700

    course page styling

--- pages/course/[course]/[...slug].vue
+++ pages/course/[course]/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto px-4 mt-20">
+  <section class="page page__course mx-auto px-4 mt-20">
     <!-- <LibraryNav :navigation-tree="navigation" /> -->
     <ContentRenderer :value="data">
       <ContentRendererMarkdown :value="data" />
@@ -24,66 +24,68 @@
 </script>
 
 <style lang="scss">
-  a {
-    @apply font-medium;
-    @apply text-blue-600;
-    @apply dark:text-blue-500;
-    @apply hover:underline;
-  }
+  .page__course {
+    a {
+      @apply font-medium;
+      @apply text-blue-600;
+      @apply dark:text-blue-500;
+      @apply hover:underline;
+    }
 
-  p {
-    @apply mb-8;
-    @apply text-base;
-    @apply md:text-lg;
-    @apply max-w-prose;
-    @apply text-body;
-    @apply leading-normal;
-  }
+    p {
+      @apply mb-8;
+      @apply text-base;
+      @apply md:text-lg;
+      @apply max-w-prose;
+      @apply text-body;
+      @apply leading-normal;
+    }
 
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    @apply mb-4;
-    @apply font-heading;
-    @apply font-semibold;
-    @apply tracking-tighter;
-    @apply leading-snug;
-    @apply text-gray-900;
-  }
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      @apply mb-4;
+      @apply font-heading;
+      @apply font-semibold;
+      @apply tracking-tighter;
+      @apply leading-snug;
+      @apply text-gray-900;
+    }
 
-  h1 > i,
-  h2 > i,
-  h3 > i,
-  h4 > i,
-  h5 > i,
-  h6 > i {
-    @apply font-serif;
-  }
+    h1 > i,
+    h2 > i,
+    h3 > i,
+    h4 > i,
+    h5 > i,
+    h6 > i {
+      @apply font-serif;
+    }
 
-  h1 {
-    @apply text-5xl;
-  }
+    h1 {
+      @apply text-5xl;
+    }
 
-  h2 {
-    @apply text-4xl;
-  }
+    h2 {
+      @apply text-4xl;
+    }
 
-  h3 {
-    @apply text-3xl;
-  }
+    h3 {
+      @apply text-3xl;
+    }
 
-  h4 {
-    @apply text-2xl;
-  }
+    h4 {
+      @apply text-2xl;
+    }
 
-  h5 {
-    @apply text-xl;
-  }
+    h5 {
+      @apply text-xl;
+    }
 
-  h6 {
-    @apply text-lg;
+    h6 {
+      @apply text-lg;
+    }
   }
 </style>
