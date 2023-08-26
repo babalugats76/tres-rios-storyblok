762cda866d175465d1bf5650c85421fcba917ce1
James Colestock <james@colestock.com>
2023-08-26 01:34:29 -0700

    more font changes

--- storyblok/FeaturedPosts.vue
+++ storyblok/FeaturedPosts.vue
@@ -6,7 +6,7 @@
         >Blog</span
       >
       <h3
-        class="mb-4 text-4xl md:text-5xl leading-tight text-darkCoolGray-900 font-heading font-bold tracking-tighter"
+        class="mb-4 text-4xl md:text-5xl leading-tight text-darkCoolGray-900 font-heading font-extrabold tracking-tighter"
       >
         {{ blok.headline }}
       </h3>
